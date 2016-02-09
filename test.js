fb9f554960f065d58e45c8ae31d18c0bea71cb44
Thomas Watson Steen <w@tson.dk>
2016-02-09 14:24:45 +0100

    standard: add space between array elements

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ test('only flatten nested objects', function (t) {
     number: 1,
     string: 'foo',
     bool: true,
-    arr1: [1,2,3],
+    arr1: [1, 2, 3],
     arr2: [{ foo: 1 }, { bar: 2 }],
     sub: { foo: 1, bar: { baz: 3 } }
   }
@@ -17,7 +17,7 @@ test('only flatten nested objects', function (t) {
     number: 1,
     string: 'foo',
     bool: true,
-    arr1: [1,2,3],
+    arr1: [1, 2, 3],
     arr2: [{ foo: 1 }, { bar: 2 }],
     'sub.foo': 1,
     'sub.bar.baz': 3
